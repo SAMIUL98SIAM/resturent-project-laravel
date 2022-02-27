231f8450196d43c34ecf61fc341b03fbbf5b21ec
SAMIUL98SIAM <samiulsiam89@gmail.com>
2022-02-28 01:17:37 +0600

    Logo SLider dynamic successfully

--- script/resources/views/layouts/frontend/partials/slider.blade.php
+++ script/resources/views/layouts/frontend/partials/slider.blade.php
@@ -1,44 +1,20 @@
 <div id="slides" class="cover-slides">
     <ul class="slides-container">
+        @foreach ($sliders as $slider)
         <li class="text-left">
-            <img src="{{asset('frontend/images/slider-01.jpg')}}" alt="">
+            <img src="{{!empty($slider->image)?url('uploads/slider_images/'.$slider->image):url('/uploads/no_image.jpg/')}}" alt="">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h1 class="m-b-20"><strong>Welcome To <br> Live Dinner Restaurant</strong></h1>
-                        <p class="m-b-40">See how your users experience your website in realtime or view  <br>
-                        trends to see any changes in performance over time.</p>
+                        <h1 class="m-b-20"><strong>Welcome To <br> {{$slider->short_title}}</strong></h1>
+                        <p class="m-b-40">{{$slider->long_title}}  <br>
+                            {{$slider->long_title_o}}</p>
                         <p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
                     </div>
                 </div>
             </div>
         </li>
-        <li class="text-left">
-            <img src="{{asset('frontend/images/slider-02.jpg')}}" alt="">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h1 class="m-b-20"><strong>Welcome To <br> Live Dinner Restaurant</strong></h1>
-                        <p class="m-b-40">See how your users experience your website in realtime or view  <br>
-                        trends to see any changes in performance over time.</p>
-                        <p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
-                    </div>
-                </div>
-            </div>
-        </li>
-        <li class="text-left">
-            <img src="{{asset('frontend/images/slider-03.jpg')}}" alt="">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h1 class="m-b-20"><strong>Welcome To <br> Yamifood Restaurant</strong></h1>
-                        <p class="m-b-40">See how your users experience your website in realtime or view  <br>
-                        trends to see any changes in performance over time.</p>
-                        <p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
-                    </div>
-                </div>
-            </div>
-        </li>
+        @endforeach
     </ul>
     <div class="slides-navigation">
         <a href="#" class="next"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
