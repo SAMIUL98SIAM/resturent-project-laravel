1601efbd5deb199595da6449cb1a2869c9bd383f
SAMIUL98SIAM <samiulsiam89@gmail.com>
2022-02-28 01:05:32 +0600

    Logo SLider dynamic successfully

--- script/app/Http/Controllers/Frontend/MenuController.php
+++ script/app/Http/Controllers/Frontend/MenuController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Logo;
-use App\Models\Slider;
+
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
     public function menu()
     {
-        return view('frontend.menu');
+        $data['logo'] = Logo::first();
+        return view('frontend.menu',$data);
     }
 }
